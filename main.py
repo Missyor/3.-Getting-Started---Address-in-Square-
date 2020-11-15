9237b59299c8070ca07b487354da7b7e0fd41a11
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 15:11:32 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#Different ways to print the same output
+# 1. using \n for a new line
+print("\t|\t--------------------\t\t|\t")
+print("\t|\tAoife O'Rourke\t\t\t\t|\t\n\t|\tCabinteely Community School\t|\t\t\n\t|\tJohnstown Road\t\t\t\t|\t\n\t|\tDublin 18\t\t\t\t\t|\t\n\t|\t--------------------\t\t|\t")
+
+# 2. using separate print statements for each line
+print("\n")     #to get a space between the 2 options
+print("\t|\t--------------------\t\t|\t")
+print("\t|\tAoife O'Rourke\t\t\t\t|\t")
+print("\t|\tCabinteely Community School\t|\t\t")
+print("\t|\tJohnstown Road\t\t\t\t|\t")
+print("\t|\tDublin 18\t\t\t\t\t|\t")
+print("\t|\t--------------------\t\t|\t")
+
